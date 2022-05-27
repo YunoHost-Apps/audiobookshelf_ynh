9be8d99cd85ab108877eadd1713376a7f9867608
yalh76 <yalh@yahoo.com>
2022-05-27 18:42:25 +0200

    Apply last example_ynh

--- conf/dev.js
+++
@@ -1,9 +0,0 @@
-const Path = require('path')
-module.exports.config = {
-  Port: __PORT__,
-  ConfigPath: Path.resolve('__DATADIR__/config'),
-  AudiobookPath: Path.resolve('__DATADIR__/audiobooks'),
-  MetadataPath: Path.resolve('__DATADIR__/metadata'),
-  FFmpegPath: '/usr/bin/ffmpeg',
-  FFProbePath: '/usr/bin/ffprobe'
-}
