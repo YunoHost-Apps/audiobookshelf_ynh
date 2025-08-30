450604945ea4d8dcef94411916d7bb6024a349d4
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-08-30 11:59:59 +0200

    nodejs resources

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,8 +3,3 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
-
-# dependencies used by the app
-
-# nodejs version
-nodejs_version=22
